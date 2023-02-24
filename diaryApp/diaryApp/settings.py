0c3ae4a57c66c2a38913a01c96b080cd0375cf99
Richard Emmanuel <richardemmanuel45@gmail.com>
2023-02-24 17:20:09 +0100

    added a gitignore rule

--- diaryApp/diaryApp/settings.py
+++ diaryApp/diaryApp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r+=bne-i8*gruwlg^%a9&#oxgmk1f6so)f=+y48y=+==s&yqm1"
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
